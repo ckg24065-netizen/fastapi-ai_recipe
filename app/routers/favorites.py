11e6d1b2fdfcc3a596a7821c81ac678187de468a
rutonn <ckg24065@st.ckg.ac.jp>
2026-02-08 16:13:49 +0900

    router,favarites API追加

--- app/routers/favorites.py
+++ app/routers/favorites.py
@@ -4,3 +4,23 @@ router = APIRouter(
     prefix="/favorites",
     tags=["Favorites"] #Swaggerでのグループ名
 )
+
+@router.get("favorites")
+async def favorites():
+    """
+    generationで登録されたレシピを一覧表示
+    DBから情報を持ってくる
+    """
+    return{"message":"お気に入り登録一覧"}
+
+@router.get("/favorite{id}")
+async def recipe():
+    """
+    お気に入りのレシピ詳細閲覧ページ
+
+    """
+    return[
+        {"message":"料理名"},
+        {"message":"材料"},
+        {"message":"作り方"},
+    ]
--- app/routers/recipes.py
+++ app/routers/recipes.py
@@ -4,3 +4,43 @@ router = APIRouter(
     prefix="/recipes",
     tags=["Recipes"] #Swaggerでのグループ名
 )
+
+#-----レシピ生成に対しての動き-----
+@router.get("/")
+async def create_recipe():
+    """
+    料理に対しての好みを入力動き
+    """
+    return[
+        {"message":"好み入力"},
+    ]
+
+@router.post("/generations")
+async def generation_recipe():
+    """
+    AIにレシピ生成
+    """
+    return {"status":"ok","message":"仮作成"}
+
+
+#----履歴に対しての動き-----
+@router.get("/recipes{id}")
+async def history():
+    """
+    DBから料理名の一覧を表示
+    """
+    return[
+        {"message":"料理名"}
+    ]
+
+@router.get("/recipe{id}")
+async def recipe():
+    """
+    履歴のレシピ詳細閲覧ページ
+
+    """
+    return[
+        {"message":"料理名"},
+        {"message":"材料"},
+        {"message":"レシピ"},
+    ]
