19fab8f41186516ddaeac1604c279cd21076f541
rutonn <ckg24065@st.ckg.ac.jp>
2026-02-25 10:40:07 +0900

    "feature: お気に入りの閲覧機能のみ追加"

--- app/routers/favorites.py
+++ app/routers/favorites.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
-from app.schema.recipe import RecipeResponse,RecipeTitleResponse
-from app.schema.favorite import FavoriteCreate,FavoriteResponse
-from app.database import get_db
+from app.schema.recipe import RecipeTitleResponse
+from app.schema.favorite import FavoriteResponse
+from app.models.favorite import Favorite
+from app.models.recipe import Recipe
+from app.database import get_db, SessionLocal
 from sqlalchemy.orm import Session
 from sqlalchemy import insert, select, delete
 
@@ -12,18 +14,17 @@ router = APIRouter(
 
 @router.get("/",response_model=list[RecipeTitleResponse])
 async def favorites(db:Session = Depends(get_db)):
-    """
-    お気に入り閲覧
-    """
-    return [
-        {"id":1,"title":"料理名"}
-    ]
+    
+    favorite_recipe = (
+        db.query(Favorite.id,Recipe.title)
+        .join(Recipe,Favorite.recipe_id == Recipe.id)
+        .all()
+    )
+    return favorite_recipe
 
 @router.post("/",response_model=FavoriteResponse)
 async def recipe(db:Session = Depends(get_db)):
-    """
-    お気に入り追加
-    """
+
     return {"id":1,"recipe_id":1,"created_at":"2025-11-20T00:00:00"}
 
 @router.delete("/{id}")
@@ -32,4 +33,18 @@ async def delete(id: int, db:Session = Depends(get_db)):
     お気に入り削除
     """
     return {"status":204,"detail":"favorite delete"}
-    
+ 
+# ---- テストデータ -----   
+@router.get("/test-favorite")
+def test_favorite(db: Session = Depends(get_db)):
+
+    new_favorite = Favorite(recipe_id=1)
+    recipe = db.query(Recipe).filter(Recipe.id == 1).first()
+    db.add(new_favorite)
+    db.commit()
+
+    return {
+        "title": recipe.title,
+        "material":recipe.material,
+        "recipe_text":recipe.recipe_text
+    }
